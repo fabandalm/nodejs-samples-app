f784a569e4a4d348875ffdb3b993c62035c07a14
Almeida <falmeida.tech@gmail.com>
2020-05-16 22:12:13 -0400

    Added middleware function

--- node-rest-sample/index.js
+++ node-rest-sample/index.js
@@ -5,6 +5,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
+app.use(function(req,res,next){
+    console.log('Logging...');
+    //next();
+})
+
 const courses = [
     {id:1, name:'Course 1'},
     {id:2, name:'Course 2'},
